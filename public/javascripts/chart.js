0b4fe3a26070cb70bab8a58f210ef8774bb398b6
Sam Starling <mail@samstarling.co.uk>
2014-08-22 11:15:20 +0100

    Allow metrics to be specified via config.json

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,51 +1,62 @@
-function initializeGraph(id, dataURL) {
-  var axes;
-  var graph = new Rickshaw.Graph.Ajax( {
-      element: document.getElementById(id).getElementsByClassName("graph")[0],
-      height: 140,
-      min: 0,
-      renderer: 'line',
-      dataURL: dataURL,
-      onComplete: function(transport) {
-        var graph = transport.graph;
-        if(!axes) {
-          axes = new Rickshaw.Graph.Axis.Time({
-            graph: graph,
-            fixedTimeUnit: 1
-          });
-          var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-              graph: graph
-          });
-        }
-        var data = graph.series[0].data;
-        var last = Math.floor(data[data.length - 1].y);
-        $("#" + id + " .stats .latest .number").text(last);
-        if(last > 750) {
-          $("#" + id + " .stats .latest .number").addClass("bad");
-        } else {
-          $("#" + id + " .stats .latest .number").addClass("good");
-        }
+// function addCommas(nStr) {
+//   nStr += '';
+//   x = nStr.split('.');
+//   x1 = x[0];
+//   x2 = x.length > 1 ? '.' + x[1] : '';
+//   var rgx = /(\d+)(\d{3})/;
+//   while (rgx.test(x1)) {
+//     x1 = x1.replace(rgx, '$1' + ',' + '$2');
+//   }
+//   return x1 + x2;
+// }
 
-
-        graph.update();
-      },
-      series: [
-        { name: 'Response Time', color: '#45B29D' }
-      ]
-  });
-
-
-  setInterval(function() {
-    graph.request();
-  }, 1000);
-}
 
 $(document).ready(function() {
+
+  function initializeGraph($metrics, namespace, metric) {
+    var axes;
+    var $metric = $($metrics[0]);
+    var graph = new Rickshaw.Graph.Ajax( {
+        element: $metric.find(".metric--graph")[0],
+        height: 140,
+        width: 300,
+        min: 'auto',
+        renderer: 'line',
+        interpolation: 'line',
+        dataURL: 'data?namespace=' + namespace + '&metric=' + metric,
+        onComplete: function(transport) {
+          var graph = transport.graph;
+          if(!axes) {
+            axes = new Rickshaw.Graph.Axis.Time({
+              graph: graph,
+              fixedTimeUnit: 1
+            });
+            var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+                graph: graph
+            });
+          }
+          var data = graph.series[0].data;
+          var last = Math.floor(data[data.length - 1].y);
+          $metric.find('.metric--value').text(last);
+          graph.update();
+        },
+        series: [
+          { name: 'data', color: '#45B29D' }
+        ]
+    });
+
+
+    setInterval(function() {
+      graph.request();
+    }, 1000);
+  }
+
+
   var formatDate = d3.time.format.iso;
-  initializeGraph('cw', 'data?id=ldp-core-response-time')
-  initializeGraph('cw-v2', 'data?id=ldp-core-cw-v2-response-time')
-  initializeGraph('cwl', 'data?id=ldp-core-cw-v2-london-response-time')
-  initializeGraph('tc', 'data?id=ldp-core-tag-concepts-response-time')
-  initializeGraph('d', 'data?id=ldp-core-datasets-response-time')
-  initializeGraph('o', 'data?id=ldp-core-ontologies-response-time')
+  $(".metric").each(function( index ) {
+    $metric = $(this);
+    var namespace = $metric.attr('data-namespace');
+    var metricName = $metric.attr('data-metric-name');
+    initializeGraph($metric, namespace, metricName);
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 var awsClient = require('../lib/awsClient');
+var config = require('../config');
 var _ = require('underscore');
 
 module.exports.index = function (req, res, next) {
-  res.render('index');
+  res.render('index', {
+    title: config.title,
+    metrics: config.metrics
+  });
 }
 
 module.exports.data = function (req, res, next) {
-  awsClient.metric(req.query.id, function(err, data) {
-    var transformedData = data.map(function(x) { return {
-      x: x.Timestamp.getTime() / 1000,
-      y: x.Average
-    }});
-
-    res.json([
-      {
-        name: "Response Time",
-        data: _.sortBy(transformedData, 'x')
+  awsClient.metric(req.query.namespace, req.query.metric, function(err, data) {
+    var transformedData = data.map(function(cur, idx) {
+      return {
+        x: cur.Timestamp.getTime() / 1000,
+        y: cur.Average
       }
-    ]);
+    });
+
+    res.json([{
+      name: "data",
+      data: _.sortBy(transformedData, 'x')
+    }]);
   });
 }
